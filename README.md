c20ba8738cc6b2af3b2f7516156c303fc154205a
Welby Seely <welbyseely@gmail.com>
2023-07-17 19:29:43 -0700

    Initial commit - added a hello-world Skeleton

---
+++ README.md
@@ -0,0 +1,1 @@
+# llama-cpp-java-bindings
---
+++ src/main/java/com/mangomelancholy/llama/cpp/java/bindings/Main.java
@@ -0,0 +1,7 @@
+package com.mangomelancholy.llama.cpp.java.bindings;
+
+public class Main {
+  public static void main(final String[] args) {
+    System.out.println("Hello there");
+  }
+}
