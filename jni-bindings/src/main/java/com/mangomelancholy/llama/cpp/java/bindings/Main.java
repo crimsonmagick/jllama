a298b15e5399ab85129cefbe08372fed210ac869
Welby Seely <welbyseely@gmail.com>
2023-07-18 20:11:43 -0700

    Added addition example binding

--- jni-bindings/src/main/java/com/mangomelancholy/llama/cpp/java/bindings/Main.java
+++ jni-bindings/src/main/java/com/mangomelancholy/llama/cpp/java/bindings/Main.java
@@ -6,8 +6,17 @@ public class Main {
     System.loadLibrary("jni-implementation");
   }
   public static void main(final String[] args) {
-    new Main().sayHello();
+    final Main main = new Main();
+
+    main.sayHello();
+
+    final int first = 1;
+    final int second = 3;
+    final long result = main.sum(first, second);
+    System.out.printf("%d + %d = %d\n", first, second, result);
   }
 
   private native void sayHello();
+
+  private native long sum(int first, int second);
 }
--- jni-implementation/src/main/cpp/library.cpp
+++ jni-implementation/src/main/cpp/library.cpp
@@ -5,3 +5,7 @@
 JNIEXPORT void JNICALL Java_com_mangomelancholy_llama_cpp_java_bindings_Main_sayHello (JNIEnv* env, jobject thisObject) {
     printf("Hello, World!\n");
  }
+
+JNIEXPORT jlong JNICALL Java_com_mangomelancholy_llama_cpp_java_bindings_Main_sum (JNIEnv* env, jobject thisObject, jint first, jint second) {
+  return (long) first + (long) second;
+}
