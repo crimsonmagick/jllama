4b34dcb3c1dce10a824093971eb1447ad3728c36
Welby Seely <welbyseely@gmail.com>
2023-07-18 22:33:25 -0700

    llama_backend_init is now being called using the binding

--- jni-bindings/src/main/java/com/mangomelancholy/llama/cpp/java/bindings/Main.java
+++ jni-bindings/src/main/java/com/mangomelancholy/llama/cpp/java/bindings/Main.java
@@ -14,9 +14,13 @@ public class Main {
     final int second = 3;
     final long result = main.sum(first, second);
     System.out.printf("%d + %d = %d\n", first, second, result);
+
+    main.initializeLlama(false);
   }
 
   private native void sayHello();
 
   private native long sum(int first, int second);
+
+  private native void initializeLlama(boolean useNuma);
 }
--- jni-implementation/src/main/cpp/library.cpp
+++ jni-implementation/src/main/cpp/library.cpp
@@ -1,11 +1,69 @@
+#include <stdio.h>
 #include "com_mangomelancholy_llama_cpp_java_bindings_Main.h"
 
-#include <stdio.h>
+#ifdef _WIN32
+#include <windows.h>
 
-JNIEXPORT void JNICALL Java_com_mangomelancholy_llama_cpp_java_bindings_Main_sayHello (JNIEnv* env, jobject thisObject) {
-    printf("Hello, World!\n");
- }
+JNIEXPORT void JNICALL Java_com_mangomelancholy_llama_cpp_java_bindings_Main_initializeLlama
+  (JNIEnv * env, jobject thisObject, jboolean useNuma) {
+    typedef void (*llama_backend_init)(bool);
+    HINSTANCE hLlamaDLL = NULL;
+    hLlamaDLL = LoadLibrary("llama.dll");
 
-JNIEXPORT jlong JNICALL Java_com_mangomelancholy_llama_cpp_java_bindings_Main_sum (JNIEnv* env, jobject thisObject, jint first, jint second) {
-  return (long) first + (long) second;
+    if (!hLlamaDLL) {
+      printf("could not load the dynamic library, error: %d\n", GetLastError());
+      return;
+    }
+
+    // resolve function address here
+    llama_backend_init func = (llama_backend_init) GetProcAddress(hLlamaDLL, "llama_backend_init");
+    if (!func) {
+      printf("could not locate the function\n");
+    } else {
+      printf("loaded function successfully!\n");
+    }
+    try {
+      func(useNuma);
+      printf("Initialized llama.cpp backend\n");
+    } catch (...) {
+      printf("WARNING - Failed to initialized llama.cpp backend");
+    }
+    FreeLibrary(hLlamaDLL);
 }
+
+#elif defined(__unix__)
+
+JNIEXPORT void JNICALL Java_com_mangomelancholy_llama_cpp_java_bindings_Main_initializeLlama
+  (JNIEnv * env, jobject thisObject, jboolean useNuma) {
+    printf("WARNING - UNIX-LIKE ARCHITECTURE NOT SUPPORTED\n");
+}
+
+#else
+JNIEXPORT void JNICALL
+Java_com_mangomelancholy_llama_cpp_java_bindings_Main_initializeLlama
+(JNIEnv
+* env,
+jobject thisObject, jboolean
+useNuma) {
+printf("WARNING - ARCHITECTURE NOT SUPPORTED\n");
+}
+#endif
+
+JNIEXPORT void JNICALL
+Java_com_mangomelancholy_llama_cpp_java_bindings_Main_sayHello (JNIEnv
+* env,
+jobject thisObject
+) {
+printf("Hello, World!\n");
+}
+
+JNIEXPORT jlong
+JNICALL Java_com_mangomelancholy_llama_cpp_java_bindings_Main_sum
+    (JNIEnv * env, jobject
+thisObject,
+jint first, jint
+second) {
+return (long) first + (long)
+second;
+}
+
