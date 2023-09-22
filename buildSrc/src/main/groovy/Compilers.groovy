d25bf38622f849fce356b9441dd3e7e677800e61
Welby Seely <welbyseely@gmail.com>
2023-09-22 12:51:28 -0400

    Updated output directory for non-Windows platforms

--- buildSrc/src/main/groovy/Compilers.groovy
+++ buildSrc/src/main/groovy/Compilers.groovy
@@ -1,17 +1,31 @@
 enum Compilers {
 
-  CLANG("Unix Makefiles", null),
-  GCC("Unix Makefiles", null),
-  MINGW("MinGW Makefiles", "/src/windows-gcc-toolchain.cmake"),
-  MSVC("Visual Studio 17 2022", "/src/windows-msvc-toolchain.cmake"),
-  NONE_FOUND(null, null)
+  CLANG("Unix Makefiles", null, "clang --version"),
+  GCC("Unix Makefiles", null, 'gcc --version'),
+  MINGW("MinGW Makefiles", "/src/windows-gcc-toolchain.cmake", "gcc --version"),
+  MSVC("Visual Studio 17 2022", "/src/windows-msvc-toolchain.cmake", "cl"),
+  NONE_FOUND(null, null, null)
 
-  final generator;
+  final generator
   final toolchain
+  final checkCommand;
 
-  Compilers(final String generator, final String toolchain) {
-    this.generator = generator;
+  Compilers(final String generator, final String toolchain, final String checkCommand) {
+    this.generator = generator
     this.toolchain = toolchain
+    this.checkCommand = checkCommand
+  }
+
+  boolean isPresent() {
+    try {
+      final output = checkCommand.execute()
+      if (this == MINGW && !output.contains('MinGW')) {
+        return false
+      }
+      return true
+    } catch (final Exception ignored) {
+      return false
+    }
   }
 
 }
