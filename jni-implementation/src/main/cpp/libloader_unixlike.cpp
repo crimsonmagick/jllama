39a1f0cacdd49fdef1bec513cb86a5ce7888e793
Welby Seely <welbyseely@gmail.com>
2023-10-03 20:20:44 -0400

    Added supporting for resource loading for *nix systems

--- jni-implementation/src/main/cpp/libloader_unixlike.cpp
+++ jni-implementation/src/main/cpp/libloader_unixlike.cpp
@@ -5,6 +5,8 @@
 #include <sstream>
 #include <iostream>
 #include "exceptions/DynamicLibraryException.h"
+#include "j_classes_consts.h"
+#include "jni.h"
 
 #define LIBRARY_PREFIX "lib"
 
@@ -28,9 +30,56 @@ void loadLibrary(std::string libName) {
         << "Could not load the dynamic library with libraryFileName="
         << libraryFileName << "error=" << dlerror();
     std::string errorMessage = errorMessageStream.str();
-    std::cerr << errorMessage << "\n";
+    std::cerr << errorMessage << std::endl;
     throw DynamicLibraryException(errorMessage.c_str());
   }
+}
+
+void loadLibrary(const std::string libname, JNIEnv* env) {
+
+  jclass dllUtil = env->FindClass(DLL_UTIL_NAME);
+  if (!dllUtil) {
+    std::string errorMessage = "Unable to find Java class with className="
+        + std::string(DLL_UTIL_NAME);
+    throw jni::JNIException(errorMessage.c_str());
+  }
+
+  jmethodID methodId = env->GetStaticMethodID(dllUtil, "extract", "(Ljava/lang/String;)Ljava/lang/String;");
+  if (!methodId) {
+    throw jni::JNIException("Unable to find method with methodName=extract");
+  }
+
+  jstring jlibName = env->NewStringUTF(libname.c_str());
+  jstring jlibPath = static_cast<jstring>(env->CallStaticObjectMethod(dllUtil, methodId, jlibName));
+
+  const char* libPath;
+  if (env->ExceptionCheck()){
+    std::cerr << "DllUtil#extract error, errorMessage=";
+    env->ExceptionDescribe();
+    std::cerr << std::endl;
+    env->ExceptionClear();
+    libPath = nullptr;
+    llamaHandle = nullptr;
+  } else {
+    libPath = env->GetStringUTFChars(jlibPath, nullptr);
+    llamaHandle = dlopen(libPath, RTLD_LAZY);
+  }
+
+  if (!llamaHandle) {
+    std::stringstream stdMessageStream;
+    std::string libraryFileName = libPath == nullptr ? "UNABLE_TO_RETRIEVE" : libPath;
+    stdMessageStream
+        << "Could not load the dynamic library with libraryFileName="
+        << libraryFileName << ", error=" << dlerror();
+    std::string errorMessage = stdMessageStream.str();
+    std::cout << errorMessage << std::endl;
+    if (libPath) {
+      env->ReleaseStringUTFChars(jlibPath, libPath);
+    }
+    throw DynamicLibraryException(errorMessage.c_str());
+  }
+
+  env->ReleaseStringUTFChars(jlibPath, libPath);
 }
 
 void closeLibrary() {
