4cb7133ca33b2e3896cce3d145f7597b376c0b8d
Welby Seely <welbyseely@gmail.com>
2023-10-04 01:48:35 -0400

    Cross-compilation Linux -> Mac appears to work

--- jni-implementation/src/macos-linux-aarch64-clang-toolchain.cmake
+++ jni-implementation/src/macos-linux-aarch64-clang-toolchain.cmake
@@ -1,15 +1,12 @@
 set(CMAKE_SYSTEM_NAME Darwin)
-set(CMAKE_SYSTEM_PROCESSOR "aarch64")
+set(CMAKE_SYSTEM_PROCESSOR "arm64e")
 
-set(CMAKE_C_COMPILER "/path/to/aarch64-linux-gnu-clang")
-set(CMAKE_CXX_COMPILER "/path/to/aarch64-linux-gnu-clang++")
+set(CMAKE_C_COMPILER "/usr/local/osxcross/bin/oa64e-clang")
+set(CMAKE_CXX_COMPILER "/usr/local/osxcross/bin/oa64e-clang++")
 
-set(CMAKE_OSX_DEPLOYMENT_TARGET "11.0")
+set(ARM64_LINKER "/usr/local/osxcross/bin/arm64e-apple-darwin22.4-ld")
+set(MAC_FRAMEWORKS "/usr/local/osxcross/SDK/MacOSX13.3.sdk/System/Library/Frameworks")
+set(MAC_TARGET "arm64e-apple-darwin")
 
-# Add any compiler flags you might need
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -target aarch64-apple-darwin")
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -target aarch64-apple-darwin")
-
-# Optionally set the sysroot if required (you need to specify the correct path)
-# set(CMAKE_OSX_SYSROOT "/path/to/MacOSX.sdk")
-
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -target ${MAC_TARGET} -fuse-ld=${ARM64_LINKER} -F${MAC_FRAMEWORKS}")
+set(CMAKE_C_FLAGS "${CMAKE_CXX_FLAGS} -target ${MAC_TARGET} -fuse-ld=${ARM64_LINKER} -F${MAC_FRAMEWORKS}")
