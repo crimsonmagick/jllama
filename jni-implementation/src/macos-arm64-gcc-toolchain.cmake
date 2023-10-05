bd977d6ef2ca793d68bdc9cef3d7200023d5fb02
Welby Seely <welbyseely@gmail.com>
2023-10-05 17:06:10 -0400

    Mac toolchains

---
+++ jni-implementation/src/macos-arm64-gcc-toolchain.cmake
@@ -0,0 +1,4 @@
+set(CMAKE_C_COMPILER "/usr/bin/gcc")
+set(CMAKE_CXX_COMPILER "/usr/bin/g++")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wextra")
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wall -Wextra")
