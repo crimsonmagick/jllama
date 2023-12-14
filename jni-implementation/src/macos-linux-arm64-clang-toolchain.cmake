1b034eb8e80c7e18a06705a81252f1d376f9030a
Welby Seely <welbyseely@gmail.com>
2023-12-14 10:45:18 -0500

    Added Dockerfile for building and publishing jllama

--- jni-implementation/src/macos-linux-arm64-clang-toolchain.cmake
+++ jni-implementation/src/macos-linux-arm64-clang-toolchain.cmake
@@ -1,11 +1,11 @@
 set(CMAKE_SYSTEM_NAME Darwin)
 set(CMAKE_SYSTEM_PROCESSOR "arm64")
 
-set(CMAKE_C_COMPILER "/usr/local/osxcross/bin/arm64-apple-darwin22.4-clang")
-set(CMAKE_CXX_COMPILER "/usr/local/osxcross/bin/arm64-apple-darwin22.4-clang++")
+set(CMAKE_C_COMPILER "/usr/local/osxcross/bin/arm64-apple-darwin21.1-clang")
+set(CMAKE_CXX_COMPILER "/usr/local/osxcross/bin/arm64-apple-darwin21.1-clang++")
 
-set(ARM64_LINKER "/usr/local/osxcross/bin/arm64-apple-darwin22.4-ld")
-set(MAC_FRAMEWORKS "/usr/local/osxcross/SDK/MacOSX13.3.sdk/System/Library/Frameworks")
+set(ARM64_LINKER "/usr/local/osxcross/bin/arm64-apple-darwin21.1-ld")
+set(MAC_FRAMEWORKS "/usr/local/osxcross/SDK/MacOSX12.0.sdk/System/Library/Frameworks/")
 set(MAC_TARGET "arm64-apple-darwin")
 
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -target ${MAC_TARGET} -fuse-ld=${ARM64_LINKER} -F${MAC_FRAMEWORKS}")
