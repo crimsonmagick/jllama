a5adadf5963d660df91564e1729869febe2beee3
Welby Seely <welbyseely@gmail.com>
2023-12-16 12:14:43 -0500

    Fix for Windows and Linux builds, fix for JAR publishing
    * Added workaround fix to publishing all impl jars
    * Updated Windows and Linux toolchains to statically link the std libraries
    * Began adding a new Windows docker image for testing purposes
      (unfinished)
    * Moved docker images into "docker" directory (from .github)

--- .github/release_jllama.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-git clone -b master https://github.com/crimsonmagick/llama-cpp-java-bindings jllama
-cd jllama
-git submodule update --init --recursive
-./gradlew build jar --info -PRELEASE
-./gradlew publish --info
--- jni-implementation/src/windows-mingw-toolchain.cmake
+++ jni-implementation/src/windows-mingw-toolchain.cmake
@@ -3,7 +3,7 @@ set(CMAKE_SYSTEM_PROCESSOR "x86_64")
 set(CMAKE_C_COMPILER "gcc")
 set(CMAKE_CXX_COMPILER "g++")
 
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -mthreads")
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -mthreads")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -mthreads -static-libgcc -static-libstdc++")
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -mthreads -static-libgcc")
 
 set(USING_MINGW_TOOLCHAIN TRUE)
