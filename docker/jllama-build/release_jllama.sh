bdd7fa542d5425bba604961af22f16210ab8e6ff
Welby Seely <welbyseely@gmail.com>
2023-12-18 22:41:15 -0500

    increment version

--- docker/jllama-build/release_jllama.sh
+++ docker/jllama-build/release_jllama.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 DEFAULT_BRANCH="master"
 BRANCH_NAME=${BRANCH_NAME:-$DEFAULT_BRANCH}
-git clone -b master https://github.com/crimsonmagick/llama-cpp-java-bindings jllama
+git clone -b $BRANCH_NAME https://github.com/crimsonmagick/llama-cpp-java-bindings jllama
 cd jllama
 git submodule update --init --recursive
 args=("$@")
 ./gradlew build jar publish -PRELEASE "${args[@]}" --info
+#./gradlew build jar publish -PRELEASE --info
