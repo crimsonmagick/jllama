59721d179d38ce585bd609094aead26873c5b43d
Welby Seely <welbyseely@gmail.com>
2023-12-18 19:21:15 -0500

    Workaround for compiling with MSVC
    CMake in llama.cpp was not detecting extension availablilty while the
    msvc toolchain file was being used. Removed it for now.

--- docker/jllama-build/release_jllama.sh
+++ docker/jllama-build/release_jllama.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-git clone -b mingw-fix https://github.com/crimsonmagick/llama-cpp-java-bindings jllama
+git clone -b master https://github.com/crimsonmagick/llama-cpp-java-bindings jllama
 cd jllama
 git submodule update --init --recursive
 ./gradlew build jar publish -PRELEASE --info
