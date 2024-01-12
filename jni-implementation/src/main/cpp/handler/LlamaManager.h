crimsonmagick/llama-cpp-java-bindings

Continued work on the new batch representation

jni-implementation/src/main/cpp/handler/LlamaManager.h
@@ -78,14 +78,22 @@ class LlamaManager {
                                     jobject jCandidates,
                                     jfloat temp);
       jobject defaultModelParams();
-      jobject llamaBatchInit(jobject jContext, jint jMaxTokenCount,
-                             jint jEmbeddingVectorSize, jint jSequenceIdLength);
+      jobject llamaBatchInitOld(jobject jContext, jint jMaxTokenCount,
+                                jint jEmbeddingVectorSize, jint jSequenceIdLength);
       void llamaBatchFree(jobject jBatch);
         void
         submitSequence(jobject jBatch, jintArray jTokens, jint jSequenceId, jint sequenceTokenIndex);
         jint evaluate(jobject jContext, jobject jBatch);
         void setCurrentTokenCount(jobject jBatch, jint currentTokenCount);
       jint decodeNative(jobject jContext, jobject jBatch);
+      void submitSequencePiece(jobject jBatch,
+                               jintArray jToken,
+                               jfloatArray jEmbd,
+                               jintArray jPos,
+                               jobjectArray jSeqId,
+                               jbooleanArray jLogits);
+      jobject
+      llamaBatchInit(jobject jContext, jint nTokens, jint embd, jint nSeqMax);
      private:
         friend class LlamaManager;
         explicit LlamaSession(JNIEnv* env, LlamaManager* outer)

jni-implementation/src/main/cpp/handler/LlamaSession.cpp
@@ -521,16 +521,29 @@ void LlamaManager::LlamaSession::llamaSampleTemperature(jobject jContext,
 }
 
 typedef llama_batch (* llama_batch_init_pointer) (int32_t n_tokens, int32_t embd, int32_t n_seq_max);
-jobject LlamaManager::LlamaSession::llamaBatchInit(jobject jContext,
-                                                   jint jMaxTokenCount,
-                                                   jint jEmbeddingVectorSize,
-                                                   jint jSequenceIdLength) {
+jobject LlamaManager::LlamaSession::llamaBatchInitOld(jobject jContext,
+                                                      jint jMaxTokenCount,
+                                                      jint jEmbeddingVectorSize,
+                                                      jint jSequenceIdLength) {
   return withJniExceptions(env, [this, jContext, jMaxTokenCount, jEmbeddingVectorSize, jSequenceIdLength] {
     auto batchInit = reinterpret_cast<llama_batch_init_pointer>(getFunctionAddress("llama_batch_init"));
     llama_batch batchStack = batchInit(jMaxTokenCount, jEmbeddingVectorSize, jSequenceIdLength);
     llama_batch* batchHeap = new llama_batch;
     std::memcpy(batchHeap, &batchStack, sizeof(llama_batch));
-    return jni::constructBatch(env, jContext, jMaxTokenCount, batchHeap);
+    return jni::constructBatchOld(env, jContext, jMaxTokenCount, batchHeap);
+  });
+}
+
+jobject LlamaManager::LlamaSession::llamaBatchInit(jobject jContext,
+                                                   jint jNTokens,
+                                                   jint embd,
+                                                   jint nSeqMax) {
+  return withJniExceptions(env, [this, jContext, jNTokens, embd, nSeqMax] {
+    auto batchInit = reinterpret_cast<llama_batch_init_pointer>(getFunctionAddress("llama_batch_init"));
+    llama_batch batchStack = batchInit(jNTokens, embd, nSeqMax);
+    llama_batch* batchHeap = new llama_batch;
+    std::memcpy(batchHeap, &batchStack, sizeof(llama_batch));
+    return jni::constructBatch(env, jContext, jNTokens, embd, nSeqMax, batchHeap);
   });
 }
 

crimsonmagick/jllama

jni-implementation/src/main/cpp/handler/LlamaManager.h
@@ -78,14 +78,22 @@ class LlamaManager {
                                     jobject jCandidates,
                                     jfloat temp);
       jobject defaultModelParams();
-      jobject llamaBatchInit(jobject jContext, jint jMaxTokenCount,
-                             jint jEmbeddingVectorSize, jint jSequenceIdLength);
+      jobject llamaBatchInitOld(jobject jContext, jint jMaxTokenCount,
+                                jint jEmbeddingVectorSize, jint jSequenceIdLength);
       void llamaBatchFree(jobject jBatch);
         void
         submitSequence(jobject jBatch, jintArray jTokens, jint jSequenceId, jint sequenceTokenIndex);
         jint evaluate(jobject jContext, jobject jBatch);
         void setCurrentTokenCount(jobject jBatch, jint currentTokenCount);
       jint decodeNative(jobject jContext, jobject jBatch);
+      void submitSequencePiece(jobject jBatch,
+                               jintArray jToken,
+                               jfloatArray jEmbd,
+                               jintArray jPos,
+                               jobjectArray jSeqId,
+                               jbooleanArray jLogits);
+      jobject
+      llamaBatchInit(jobject jContext, jint nTokens, jint jEmbd, jint nSeqId);
      private:
         friend class LlamaManager;
         explicit LlamaSession(JNIEnv* env, LlamaManager* outer)

jni-implementation/src/main/cpp/handler/LlamaSession.cpp
@@ -521,16 +521,29 @@ void LlamaManager::LlamaSession::llamaSampleTemperature(jobject jContext,
 }
 
 typedef llama_batch (* llama_batch_init_pointer) (int32_t n_tokens, int32_t embd, int32_t n_seq_max);
-jobject LlamaManager::LlamaSession::llamaBatchInit(jobject jContext,
-                                                   jint jMaxTokenCount,
-                                                   jint jEmbeddingVectorSize,
-                                                   jint jSequenceIdLength) {
+jobject LlamaManager::LlamaSession::llamaBatchInitOld(jobject jContext,
+                                                      jint jMaxTokenCount,
+                                                      jint jEmbeddingVectorSize,
+                                                      jint jSequenceIdLength) {
   return withJniExceptions(env, [this, jContext, jMaxTokenCount, jEmbeddingVectorSize, jSequenceIdLength] {
     auto batchInit = reinterpret_cast<llama_batch_init_pointer>(getFunctionAddress("llama_batch_init"));
     llama_batch batchStack = batchInit(jMaxTokenCount, jEmbeddingVectorSize, jSequenceIdLength);
     llama_batch* batchHeap = new llama_batch;
     std::memcpy(batchHeap, &batchStack, sizeof(llama_batch));
-    return jni::constructBatch(env, jContext, jMaxTokenCount, batchHeap);
+    return jni::constructBatchOld(env, jContext, jMaxTokenCount, batchHeap);
+  });
+}
+
+jobject LlamaManager::LlamaSession::llamaBatchInit(jobject jContext,
+                                                   jint jNTokens,
+                                                   jint jEmbd,
+                                                   jint nSeqId) {
+  return withJniExceptions(env, [this, jContext, jNTokens, jEmbd, nSeqId] {
+    auto batchInit = reinterpret_cast<llama_batch_init_pointer>(getFunctionAddress("llama_batch_init"));
+    llama_batch batchStack = batchInit(jNTokens, jEmbd, nSeqId);
+    llama_batch* batchHeap = new llama_batch;
+    std::memcpy(batchHeap, &batchStack, sizeof(llama_batch));
+    return jni::constructBatch(env, jContext, batchHeap, jNTokens, jEmbd, nSeqId);
   });
 }
 