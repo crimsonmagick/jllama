4823dcd3184466e944a943b5f966780c6c181be0
Welby Seely <welbyseely@gmail.com>
2024-01-21 13:31:49 -0500

    Attempts at fixining detokeniation. An API change is needed to handle partial UTF-8 characters

--- jni-bindings/src/main/java/net/jllama/api/Tokens.java
+++ jni-bindings/src/main/java/net/jllama/api/Tokens.java
@@ -1,6 +1,9 @@
 package net.jllama.api;
 
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import net.jllama.api.util.IntegerUtil;
@@ -12,19 +15,38 @@ import org.apache.logging.log4j.Logger;
 public class Tokens {
 
   final static Logger log = LogManager.getLogger(Tokens.class);
+  final private static byte UTF8_SPACE = " ".getBytes(StandardCharsets.UTF_8)[0];
   private final LlamaModel llamaModel;
 
   Tokens(final LlamaModel llamaModel) {
     this.llamaModel = llamaModel;
   }
 
-  public String detokenize(final List<Integer> tokens) {
-    return tokens.stream()
-        .map(this::detokenize)
-        .collect(Collectors.joining());
+  public String detokenize(final int token) {
+    return detokenize(Collections.singletonList(token));
   }
 
-  public String detokenize(final int token) {
+  public String detokenize(final List<Integer> tokens) {
+    final List<Byte> buffer = new ArrayList<>();
+    boolean leadingSpace = true;
+    for (int token : tokens) {
+      final byte[] detokenized = detokenizeUtf8(token);
+      if (leadingSpace && detokenized.length == 1 && detokenized[0] == UTF8_SPACE) {
+        leadingSpace = false;
+      } else {
+        for (byte piece : detokenized) {
+          buffer.add(piece);
+        }
+      }
+    }
+    final byte[] utf8String = new byte[buffer.size()];
+    for (int i = 0; i < buffer.size(); i++) {
+      utf8String[i] = buffer.get(i);
+    }
+    return new String(utf8String, StandardCharsets.UTF_8);
+  }
+
+  private byte[] detokenizeUtf8(final int token) {
     byte[] buf = new byte[8];
     int length = llamaModel.llamaDetokenize(token, buf);
     if (length < 0) {
@@ -36,8 +58,11 @@ public class Tokens {
       throw new RuntimeException(
           "Unable to allocate a large enough buffer for detokenized string length.");
     }
-    return new String(buf, 0, length, StandardCharsets.UTF_8);
+    byte[] rawUtf8String = new byte[length];
+    System.arraycopy(buf, 0, rawUtf8String, 0, length);
+    return rawUtf8String;
   }
+
 
   public int bos() {
     return llamaModel.llamaTokenBos();
@@ -58,19 +83,28 @@ public class Tokens {
   public List<Integer> tokenize(final String toTokenize, boolean addBos, boolean special) {
     final int estimatedLength = toTokenize.length() + (addBos ? 1 : 0);
     final int[] temp = new int[estimatedLength];
-    final int reqLength = llamaModel.llamaTokenize(toTokenize.getBytes(StandardCharsets.UTF_8), temp,
-        estimatedLength, addBos, special);
-    final int length;
-    if (reqLength < 0) {
+    final int requiredLength = llamaModel.llamaTokenize(toTokenize.getBytes(StandardCharsets.UTF_8),
+        temp, estimatedLength, addBos, special);
+    final int[] tokenized;
+    if (requiredLength < 0) {
       log.trace(
           () -> String.format("Reserved %d ints for estimatedLength, but requiredLength was %d",
-              estimatedLength, reqLength));
-      length = Math.abs(reqLength);
+              estimatedLength, requiredLength));
+      final int length = Math.abs(requiredLength);
+      tokenized = new int[length];
+      final int secondTokenizationResult = llamaModel.llamaTokenize(
+          toTokenize.getBytes(StandardCharsets.UTF_8), tokenized, length, addBos, special);
+      if (secondTokenizationResult < 0) {
+        final String errorMessage = String.format(
+            "Reserved %d ints based on first tokenization attempt, but but second attempt failed with a return value of %d",
+            estimatedLength, length);
+        log.error(errorMessage);
+        throw new LlamaCppException(errorMessage);
+      }
     } else {
-      length = reqLength;
+      tokenized = new int[requiredLength];
+      System.arraycopy(temp, 0, tokenized, 0, requiredLength);
     }
-    final int[] tokenized = new int[length];
-    System.arraycopy(temp, 0, tokenized, 0, length);
     return IntegerUtil.toList(tokenized);
   }
 }
